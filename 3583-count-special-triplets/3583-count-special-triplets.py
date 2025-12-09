552a091c95e59e81c35921a47ff24cbff8e24fc3
Anubhav Utkarsh <122220530+anu0055@users.noreply.github.com>
2025-12-09 22:55:02 +0530

    Time: 569 ms (78.83%), Space: 41.4 MB (77.93%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.py
+++ 3583-count-special-triplets/3583-count-special-triplets.py
@@ -1,18 +1,40 @@
 class Solution:
     def specialTriplets(self, nums: List[int]) -> int:
-        mod = 10**9+7
-        count = defaultdict(list)
+        mod = 10**9 + 7
+        right = defaultdict(int)
+        left = defaultdict(int)
         res = 0
-        for i, n in enumerate(nums):
-            count[n].append(i)
+
+        # total frequency
+        for n in nums:
+            right[n] += 1
+
         for j in range(len(nums)):
-            low_idx = 0
-            high_idx = 0
-            if nums[j]*2 in count:
-                for idx in count[nums[j]*2]:
-                    if idx < j:
-                        low_idx += 1
-                    if idx > j:
-                        high_idx += 1
-            res += (low_idx * high_idx)%mod
-        return res%mod
+            # current j is moving from right to left
+            right[nums[j]] -= 1
+
+            target = nums[j] * 2
+            if target in left and target in right:
+                res = (res + left[target] * right[target]) % mod
+
+            left[nums[j]] += 1
+
+        return res
+    # Brute force with 1114/1121 test passing
+    #def specialTriplets(self, nums: List[int]) -> int:
+        # mod = 10**9+7
+        # count = defaultdict(list)
+        # res = 0
+        # for i, n in enumerate(nums):
+        #     count[n].append(i)
+        # for j in range(len(nums)):
+        #     low_idx = 0
+        #     high_idx = 0
+        #     if nums[j]*2 in count:
+        #         for idx in count[nums[j]*2]:
+        #             if idx < j:
+        #                 low_idx += 1
+        #             if idx > j:
+        #                 high_idx += 1
+        #     res += (low_idx * high_idx)%mod
+        # return res%mod
