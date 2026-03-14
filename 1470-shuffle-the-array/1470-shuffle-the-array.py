763ecaa0e652e303b53591f1868233ecc15aa340
Anubhav Utkarsh <122220530+anu0055@users.noreply.github.com>
2026-03-14 20:43:04 +0530

    Time: 62 ms (68.57%), Space: 19.3 MB (97.12%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        i = 0
+        j = n
+        res = []
+        while j < 2*n and i < j:
+            res.append(nums[i])
+            res.append(nums[j])
+            i += 1
+            j += 1
+        return res
